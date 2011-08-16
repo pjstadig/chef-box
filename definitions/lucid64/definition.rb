b7989bffea8cc38510c8f30a3c544742c9a43b41
Tim Dysinger <tim@dysinger.net>
2011-08-16 09:05:46 -1000

    upgraded to 10.04.3 LTS

--- definitions/lucid64/definition.rb
+++ definitions/lucid64/definition.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + "/../.common/definition.rb"
 
 options = {
   :os_type_id => "Ubuntu_64",
-  :iso_file => "ubuntu-10.04.2-server-amd64.iso",
-  :iso_src => "http://releases.ubuntu.com/10.04.2/ubuntu-10.04.2-server-amd64.iso",
-  :iso_md5 => "b446b36430a29f3dcf0c5eb797197cac"
+  :iso_file => "ubuntu-10.04.3-server-amd64.iso",
+  :iso_src => "http://releases.ubuntu.com/10.04.3/ubuntu-10.04.3-server-amd64.iso",
+  :iso_md5 => "84b43b7bbee85d0af8e11b778c8d1290"
 }
 
 Veewee::Session.declare(SESSION.merge(options))
